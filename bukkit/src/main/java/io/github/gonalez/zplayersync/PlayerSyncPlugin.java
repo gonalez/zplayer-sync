e4fe92d063adeea5565e5064c16d09839bf9cbd8
BuildTools <gastongaga202@gmail.com>
2022-12-12 23:59:24 -0300

    Initial commit

---
+++ bukkit/src/main/java/io/github/gonalez/zplayersync/PlayerSyncPlugin.java
@@ -0,0 +1,10 @@
+package io.github.gonalez.zplayersync;
+
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class PlayerSyncPlugin extends JavaPlugin implements Listener {
+
+  @Override
+  public void onEnable() {}
+}
